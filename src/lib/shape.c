b9f7f9383ef94733c35e5a11add21304a2c7972c
bitskiwi <varkiwibusiness@gmail.com>
2024-04-30 18:36:51 -0700

    added points and line drawing

--- src/lib/shape.c
+++ src/lib/shape.c
@@ -24,13 +24,13 @@ struct point new_point(int x, int y){                                          /
 struct canvas draw_line(struct canvas surface, struct point p1, struct point p2){        // create a function to draw line to canvas
 	int dx = p2.x - p1.x;                                                      // define distance between first and last x coordinate
 	int dy = p2.y - p1.y;                                                      // define distance between first and last y coordinate
-	for(int x = p1.x; x < p2.x; x++){                                          // iterate through points from p1 to p2
-		int y = p1.y + dy * (x - p1.x) / dx;
-		surface = draw_canvas(surface, '#', x, y);
+	for(int x = p1.x; x < p2.x; x++){                                          // iterate through x values from p1 to p2
+		int y = p1.y + dy * (x - p1.x) / dx;                                   // use the distance to find the next corrisponding y value for x
+		surface = draw_canvas(surface, '#', x, y);                             // draw the point
 	}
-	for(int y = p1.y; y < p2.y; y++){                                          // iterate through points from p1 to p2
-		int x = p1.x + dx * (y - p1.y) / dy;
-		surface = draw_canvas(surface, '#', x, y);
+	for(int y = p1.y; y < p2.y; y++){                                          // iterate through y values from p1 to p2 (to fill in the gaps)
+		int x = p1.x + dx * (y - p1.y) / dy;                                   // use the distance to find the next corrisponding x value for y
+		surface = draw_canvas(surface, '#', x, y);                             // draw the point
 	}
-	return surface;
+	return surface;                                                            // return modified canvas
 }
--- src/main.c
+++ src/main.c
@@ -6,11 +6,11 @@
 // MAIN
 
 int main(){                                                                    // define main function
-	struct canvas surface = new_canvas(10,10);                                 // create a canvas 30x30
+	struct canvas surface = new_canvas(10,10);                                 // create a canvas 10x10
 	surface = reset_canvas(surface);                                           // fill canvas with blank chars
 	surface = draw_canvas(surface, 'B', 2, 1);                                 // write to surface 'A' at (1,1)
 	struct point p1 = new_point(0,0);                                          // make point (0,0)
-	struct point p2 = new_point(10,0);                                         // make point (1,20)
+	struct point p2 = new_point(10,10);                                        // make point (10,10)
 	surface = draw_line(surface, p1, p2);                                      // draw line from p1 to p2 on canvas
 	render_canvas(surface);                                                    // render the canvas
 }
